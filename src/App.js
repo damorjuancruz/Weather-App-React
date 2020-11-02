01332514c1c4d083c3d2dbf826c81f7f176830e7
Juan <juan@pop-os.localdomain>
2020-11-02 20:14:27 -0300

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>dou</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+import './tailwind.output.css';
+
+export default function App() {
+
+    // useEffect(() => {
+    //     axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/11222?apikey=R534nJFev2IZTQZSkZVFvwfRe63KJ9wR&metric=true').then(resp => {
+    //     let date = new Date(resp.data.DailyForecasts[0].Date);
+    //     console.log(date.toLocaleDateString());
+    // });}, [])
+
+    return (
+        <div className='container'>
+            <div className="row m-auto w-50 text-center align-items-center">
+                <div className="col p-1 border border-secondary rounded m-1">
+                    hello
+                </div>
+                <div className="col p-1 border rounded m-1">
+                    hola
+                </div>
+                <div className="col p-1 border rounded m-1">
+                    hola
+                </div>
+                <div className="col p-1 border rounded m-1">
+                    hola
+                </div>
+                <div className="col p-1 border rounded m-1">
+                    hola
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default App;
