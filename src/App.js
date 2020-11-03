a050aad1c264dc21c10c1a11c979ec29866f3585
Juan <juan@pop-os.localdomain>
2020-11-03 11:48:24 -0300

    Final Version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import WeatherCard from './components/WeatherCard';
 
 import axios from 'axios';
 
@@ -6,31 +8,30 @@ import './tailwind.output.css';
 
 export default function App() {
 
-    // useEffect(() => {
-    //     axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/11222?apikey=R534nJFev2IZTQZSkZVFvwfRe63KJ9wR&metric=true').then(resp => {
-    //     let date = new Date(resp.data.DailyForecasts[0].Date);
-    //     console.log(date.toLocaleDateString());
-    // });}, [])
+    const [forecasts, setForecasts] = useState([]);
+    const [isNight, setIsNight] = useState(false);
+
+    useEffect(() => {
+        axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/11222?apikey=R534nJFev2IZTQZSkZVFvwfRe63KJ9wR&metric=true').then(resp => {
+            setForecasts(resp.data.DailyForecasts);
+        });
+    }, []);
+
+    function handleClick() {
+        setIsNight(!isNight);
+    }
 
     return (
-        <div className='container'>
-            <div className="row m-auto w-50 text-center align-items-center">
-                <div className="col p-1 border border-secondary rounded m-1">
-                    hello
-                </div>
-                <div className="col p-1 border rounded m-1">
-                    hola
-                </div>
-                <div className="col p-1 border rounded m-1">
-                    hola
-                </div>
-                <div className="col p-1 border rounded m-1">
-                    hola
-                </div>
-                <div className="col p-1 border rounded m-1">
-                    hola
-                </div>
+        <div className={`${isNight ? "bg-gray-900 text-white" : ""}`}>
+            <div className="content-center grid grid-cols-5 gap-4 mx-auto w-1/2 sm:w-11/12 md:w-2/3 lg:w-1/2 h-screen">
+                <h1 className="col-span-5 text-center font-bold text-xl">Next 5 days forecast</h1>
+                {forecasts.map((F, index) =>
+                    <WeatherCard forecast={F} isFirst={index === 0} isNight={isNight} key={index.toString()} />)}
+                <button className="col-span-5 sm:col-start-3 sm:col-span-1 bg-blue-700 hover:bg-blue-600 focus:outline-none border rounded-xl p-1 text-white" 
+                onClick={handleClick}>
+                    {isNight ? "Day" : "Night"}
+                </button>
             </div>
         </div>
-    )
+    );
 }
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export default function WeatherCard() {
+export default function WeatherCard({ forecast, isFirst, isNight }) {
+    let date = new Date(forecast.Date);
+    var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = dayNames[date.getDay()];
+    // console.log(forecast);
+
+    let iconNumber = isNight ? forecast.Night.Icon : forecast.Day.Icon;
+    let iconURL = `https://developer.accuweather.com/sites/default/files/${iconNumber >= 10 ?
+            iconNumber.toString() :
+            "0" + iconNumber.toString()}-s.png`;
+
+    let max = forecast.Temperature.Maximum.Value.toString();
+    let min = forecast.Temperature.Minimum.Value.toString();
+
     return (
-        <div>
-            
+        <div className={`col col-span-5 sm:col-span-1 text-center border rounded-xl p-1 
+        ${isFirst ? (isNight ? "border-white" : "border-black") : (isNight ? "border-gray-700" : "border-gray-500")}`}>
+            <i>{day}</i>
+            <img src={iconURL} className="m-auto" />
+            <p>{max} | {min}</p>
         </div>
     )
 }
