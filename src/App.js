561353ce6883dd1e798b2a243ea2619e0b8cbc43
Juan <juan@pop-os.localdomain>
2020-11-03 14:06:32 -0300

    Final corrected responsive background

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ export default function App() {
 
     return (
         <div className={`${isNight ? "bg-gray-900 text-white" : ""} transition-all duration-200`}>
-            <div className="content-center grid grid-cols-5 gap-4 mx-auto w-1/2 sm:w-11/12 md:w-2/3 lg:w-1/2 h-screen">
+            <div className=" py-10 sm:py-0 sm:content-center grid grid-cols-5 gap-4 mx-auto w-1/2 sm:w-11/12 md:w-2/3 lg:w-1/2 min-h-screen">
                 <h1 className="col-span-5 text-center font-bold text-xl">5 days forecast</h1>
                 {forecasts.map((F, index) =>
                     <WeatherCard forecast={F} isFirst={index === 0} isNight={isNight} key={index.toString()} />)}
-                <button className="col-span-5 sm:col-start-3 sm:col-span-1 bg-blue-700 hover:bg-blue-600 focus:outline-none border rounded-xl p-1 text-white" 
-                onClick={handleClick}>
+                <button className="col-span-5 sm:col-start-3 sm:col-span-1 bg-blue-700 hover:bg-blue-600 focus:outline-none border rounded-xl p-1 text-white"
+                    onClick={handleClick}>
                     {isNight ? "Day" : "Night"}
                 </button>
             </div>
